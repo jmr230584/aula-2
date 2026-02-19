3709481b5bee3e065fcf544586bad645ddd93d48
JMR_996 <mateusjoseph@gmail.com>
2026-02-19 11:33:36 -0300

    Entregando Ex1, 2 e 3

---
+++ Exercicio2.cs
@@ -0,0 +1,12 @@
+// -- Arquivo Exercicio2.cs
+public static class Exercicio2
+{
+    //private static string nome = "Jose Mateus";
+
+    public static void Executar()
+    {
+        string nome = "Jose Mateus";
+        Console.WriteLine(nome);
+        Console.WriteLine();
+    }
+}
